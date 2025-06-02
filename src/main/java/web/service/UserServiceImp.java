e4fe1bddb8f1cf81aaed2d20d4e0dd3efc1904bf
Sergey <iamrussia@mail.ru>
2025-06-02 17:08:35 +0700

    дебаг сервисного слоя

---
+++ src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,69 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service // Обозначение класса как сервиса Spring
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserDao userdao;
+
+    public void createUsersTable() {
+        this.userdao.createUsersTable();
+    }
+
+    public void dropUsersTable() {
+        this.userdao.dropUsersTable();
+    }
+
+    public void saveUser(String firstname, String lastName) {
+        this.userdao.saveUser(firstname, lastName);
+        System.out.println("User с именем — " + firstname + " добавлен в базу данных.");
+    }
+
+    public void removeUserById(long id) {
+        this.userdao.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return this.userdao.getAllUsers();
+    }
+
+    public void cleanUsersTable() {
+        this.userdao.cleanUsersTable();
+    }
+
+    private final List<User> users = new ArrayList<>(); // список машин
+
+    public UserServiceImp() {
+        // Инициализация списка машин
+        users.add(new User("Иван", "Петров"));
+        users.add(new User("Григорий", "Смирнов"));
+        users.add(new User("Пётр", "Кротов"));
+        users.add(new User("Анастасия", "Сидорова"));
+        users.add(new User("Вероника", "Иванова"));
+    }
+
+    @Transactional
+    @Override
+    public void add(User user) {
+        // Сохраняем пользователя
+        userDao.add(user);
+    }
+
+
+    @Override
+    public List<User> getAllUsers(int count) {
+        if (count >= users.size()) {
+            return new ArrayList<>(users); // возвращаем весь список, если запрошенное количество больше или равно размера списка
+        } else {
+            return new ArrayList<>(users.subList(0, count)); // возвращаем подсписок из первых count элементов
+        }
+    }
+}
