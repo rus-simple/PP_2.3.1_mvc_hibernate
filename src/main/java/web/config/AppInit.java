af544babb8353eb0e62ab01422d9307dbe237de3
Sergey <iamrussia@mail.ru>
2025-06-03 19:44:45 +0700

    решена проблема неверной кодировки при записи в БД

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,8 +1,24 @@
 package web.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext aContext) throws ServletException {
+        super.onStartup(aContext);
+
+        // Добавляем фильтр для кодировки, чтобы он был первым
+        FilterRegistration.Dynamic encodingFilter = aContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        // Маппим на все URL, чтобы применялся ко всем запросам
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
 
     // Метод, указывающий на класс конфигурации
     @Override
